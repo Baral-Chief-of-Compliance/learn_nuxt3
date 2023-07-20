f87954dca44cfef906e90e53d8158c3cb89027de
unknown <dartmol21@mail.ru>
2023-07-20 13:01:07 +0300

    lesson 6

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,6 +1,48 @@
+
+
 <template>
-    <div>events page</div>
+    <div class="container px-3 py-3 mx-auto">
+        <h1 class="text-teal-500">events page</h1>
+    </div>
     <Alert />
     <Profile />
     <IconsNasa />
 </template>
+
+
+<script>
+
+export default{
+    data(){
+        return{
+            greetings: "hello from options API"
+        }
+    },
+
+    methods: {
+        say_hello(){
+            console.log(this.greetings)
+        }
+    },
+
+    beforeCreate(){
+        console.log("hello from options API beforeCretate")
+    },
+
+    created(){
+        this.say_hello()
+    },
+
+    setup(){
+
+        const { sayHello } = useUtils()
+
+
+        onMounted(() => {
+            console.log("Hello from composition API on Mounted")
+        }
+        )
+        sayHello()
+    }
+}
+</script>
