54e1d85acf2fa92625820f5d0cc796e3b8cd873c
unknown <dartmol21@mail.ru>
2023-07-20 14:47:28 +0300

    lesson 8 plugins

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,20 @@
+<script setup>
+const { $hello } = useNuxtApp()
+console.log(useNuxtApp())
+</script>
+
 <template>
     <div>index page</div>
     <div class="Main">
         <HeaderComponent />
         <Alert />
+        <div>
+            {{ $hello('world') }}
+        </div>
+
         <img src="@/assets/nasa.png" width="100" alt="NasaLogo"/>
         <img src="@/assets/nasa_2.png" width="100" alt="NasaLogo2"/>
         <img src="/nasa_2.png" width="100" alt="NasaLogo2Public"/>
+
     </div>
 </template>
